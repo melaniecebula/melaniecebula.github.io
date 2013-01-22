7e4ca0d3ca4cac92acf23764c3771a1640f0fb23
Ajay Tripathy <aurotripathy@Mackenzie.local>
2013-01-21 20:25:57 -0800

    making personal website

---
+++ melanie_website/server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+app.configure(function() {
+	app.use(express.compress());
+	app.set("views", __dirname + "/views");
+	app.set("view engine", "ejs");
+	app.use(express["static"] (__dirname + "/static"));
+	app.use(app.router);
+});
+
+app.get("/", function(req,res) {
+	res.render("index.ejs");
+});
+
+app.listen(80);
+console.log('Listening on port 8080');
